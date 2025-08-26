dd969eb7ff1282d298de1b733e789281a64709e9
Adam Mammadov <mammadovadem842@gmail.com>
2025-08-26 11:23:01 +0400

    Delete task-1-5 directory

--- task-1-5/TASK1.py
+++
@@ -1,8 +0,0 @@
-# 1-ci Task: 5-ə bölünmə yoxlaması
-num1 = int(input("1-ci task: Bir ədəd daxil edin: "))
-if num1 % 5 == 0:
-    print("5-ə bölünür")
-else:
-    print("5-ə bölünmür")
-
-print("-" * 40)
--- task-1-5/TASK2.py
+++
@@ -1,7 +0,0 @@
-# 2-ci Task: 3 ədədin ən böyüyü
-a = int(input("2-ci task: 1-ci ədədi daxil edin: "))
-b = int(input("2-ci task: 2-ci ədədi daxil edin: "))
-c = int(input("2-ci task: 3-cü ədədi daxil edin: "))
-print("Ən böyük:", max(a, b, c))
-
-print("-" * 40)
